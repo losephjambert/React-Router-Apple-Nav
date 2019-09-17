15fa0dc83c7dc226ea8074bf1bfef2eea3f8cc8e
Joe <josephc.lambert@gmail.com>
2019-09-17 09:48:17 -0700

    moves subnav to its own route and out of the page component

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 
 import NavContainer from './components/Nav/NavContainer';
+import SubNav from './components/Nav/SubNav';
 import Page from './components/Pages/page';
 import HomePage from './components/Pages/HomePage';
 
@@ -9,7 +10,7 @@ import { mainNavItems, subNavItems } from './api/navDataStub';
 
 function App() {
   const [subNav, setSubNav] = useState([]);
-  const [navItems, setNavItems] = useState(mainNavItems);
+  const [navItems] = useState(mainNavItems);
 
   const onNavChange = title => {
     setSubNav(subNavItems[title]);
@@ -19,10 +20,8 @@ function App() {
     <>
       <NavContainer navItems={navItems} onNavChange={onNavChange} />
       <Route exact path='/' component={HomePage} />
-      <Route
-        path='/:id'
-        render={routerProps => <Page {...routerProps} title={routerProps.match.params.id} subNav={subNav} />}
-      />
+      <Route path='/:id' render={routerProps => <SubNav {...routerProps} subNav={subNav} />} />
+      <Route path='/:id' render={routerProps => <Page {...routerProps} title={routerProps.match.params.id} />} />
     </>
   );
 }
--- apple-nav/src/components/Nav/SubNav.js
+++ apple-nav/src/components/Nav/SubNav.js
@@ -3,13 +3,11 @@ import React from 'react';
 const SubNav = ({ subNav }) => {
   return (
     <>
-      {subNav && (
-        <ul>
-          {subNav.map((item, i) => (
-            <li key={`_${i}_`}>{item.title}</li>
-          ))}
-        </ul>
-      )}
+      <ul>
+        {subNav.map((item, i) => (
+          <li key={`_${i}_`}>{item.title}</li>
+        ))}
+      </ul>
     </>
   );
 };
