f66a4ec1a0ced38efc65160f4d6b480ba60af680
Joe <josephc.lambert@gmail.com>
2019-09-17 08:51:21 -0700

    displays stubbed data nav items. wraps app in router.

---
+++ apple-nav/src/components/NavContainer.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import { mainNavItems, subNavItems } from '../api/navDataStub';
+
+export default function NavContainer() {
+  const [navItems, setNavItems] = useState(mainNavItems);
+  const [subNavItemDisplay, setSubNavItemDisplay] = useState();
+
+  return (
+    <ul>
+      {navItems.map(({ title, href }, i) => (
+        <li key={`_${i}_`}>
+          <Link to={href.toLowerCase()}>{title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
