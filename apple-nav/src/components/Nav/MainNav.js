1358024d393e25963b6a1ed13fcce1786db04310
Joe <josephc.lambert@gmail.com>
2019-09-18 13:00:56 -0700

    base styles for both navs

--- apple-nav/src/components/Nav/MainNav.js
+++ apple-nav/src/components/Nav/MainNav.js
@@ -2,14 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
+import { StyledNavWrapper } from './StyledNav';
+const StyledMainNav = styled.div`
+  background-color: #313131;
+  color: ghostwhite;
+  nav {
+    display: flex;
+    justify-content: space-around;
+    padding: 0 15%;
+    a {
+      padding: 10px 0;
+    }
+  }
+`;
 export default function NavContainer({ mainNavItems }) {
   return (
-    <nav>
-      {mainNavItems.map(item => (
-        <NavLink key={item.id} exact activeClassName='active' to={`/${item.url.toLowerCase()}`}>
-          {item.title}
-        </NavLink>
-      ))}
-    </nav>
+    <StyledMainNav>
+      <StyledNavWrapper>
+        {mainNavItems.map(item => (
+          <NavLink key={item.id} activeClassName='active' to={`/${item.url.toLowerCase()}`}>
+            {item.title}
+          </NavLink>
+        ))}
+      </StyledNavWrapper>
+    </StyledMainNav>
   );
 }
